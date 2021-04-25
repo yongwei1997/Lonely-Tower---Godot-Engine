de4c472ac8ee6bdc46457cc02d472667f9fd522c
looiyaowei <looiyaowei@gmail.com>
2021-04-25 16:39:13 +0800

    looi: leaderboard-changes

--- scripts/Leaderboard.gd
+++ scripts/Leaderboard.gd
@@ -3,22 +3,18 @@ extends Control
 onready var titleSize = 20
 onready var labelSize = 31
 
-var scenes = ['', 'res://Scene/leaderboard.tscn', '']
-
 func _ready():
 	setFontSize()
-	#onClickHandler()
-
-
-#func onClickHandler():
-	#var menu_labels = get_tree().get_nodes_in_group('menu_label')
+	back()
 	
-	#for index in range(menu_labels.size()):
-		#menu_labels[index].get_parent().get_parent().get_child(1).setButton(scenes[index])
-		
+
 
 func setFontSize():
 	var menu_lables = get_tree().get_nodes_in_group('menu_label')
 	
 	for label in menu_lables:
 		label.get('custom_fonts/font').set_size(labelSize)
+		
+func back():
+	var back = get_tree().get_current_scene().get_node("MarginContainer2/TextureRect/Button")
+	back.setButton('res://actors/Menu.tscn')
