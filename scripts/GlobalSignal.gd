95fec5ea8b188d3fdc4877291b987225882cb4fa
yongwei1997 <56120835+yongwei1997@users.noreply.github.com>
2021-05-09 17:26:29 +0800

    Add files via upload
    Add signal and singleton (failed)

--- scripts/GlobalSignal.gd
+++ scripts/GlobalSignal.gd
@@ -1,3 +1,18 @@
 extends Node
 
 signal bullet_fired(bullet, position, direction)
+
+onready var enemy = preload("res://actors/Enemy.tscn")
+
+var points 
+
+func _ready():
+	print("HAHA")
+	var mob_instance = enemy.instance()
+	mob_instance.connect("goblin_defeated", self, "goblin_defeated")
+	
+func goblin_defeated():
+	points += 10
+	$UI/Points.text = str(points)
+	print(points)
+	
--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
+var points = 0
 
 onready var bullet_manager = $BulletManager
 onready var player: Player = $Player
 
 func _ready():
 	GlobalSignal.connect("bullet_fired", bullet_manager, "handle_bullet_spawned") 
+
 
 
